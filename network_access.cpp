918e9d64b5e1444bae3b4984b5a41a8954fb60f3
Christophe de Dinechin <christophe@dinechin.org>
2019-03-22 13:07:10 +0100

    Automatic copyright header adjustment

--- network_access.cpp
+++ network_access.cpp
@@ -1,22 +1,42 @@
-// ****************************************************************************
-//  network_access.cpp                                             Tao project
-// ****************************************************************************
-// 
-//   File Description:
-// 
+// *****************************************************************************
+// network_access.cpp                                              Tao3D project
+// *****************************************************************************
+//
+// File description:
+//
 //    Functions to get data from the network over a URL
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// ****************************************************************************
-//  (C) 2011 Christophe de Dinechin <christophe@taodyne.com>
-//  (C) 2011 Taodyne SAS
-// ****************************************************************************
+//
+//
+//
+//
+//
+//
+//
+//
+// *****************************************************************************
+// This software is licensed under the GNU General Public License v3
+// (C) 2012-2013, Baptiste Soulisse <baptiste.soulisse@taodyne.com>
+// (C) 2012-2013, Catherine Burvelle <catherine@taodyne.com>
+// (C) 2012-2013,2019, Christophe de Dinechin <christophe@dinechin.org>
+// (C) 2012-2013, Jérôme Forissier <jerome@taodyne.com>
+// (C) 2012-2013, Baptiste Soulisse <baptiste.soulisse@taodyne.com>
+// *****************************************************************************
+// This file is part of Tao3D
+//
+// Tao3D is free software: you can r redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Tao3D is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Tao3D, in a file named COPYING.
+// If not, see <https://www.gnu.org/licenses/>.
+// *****************************************************************************
 
 #include "network_access.h"
 #include "tao/tao_utf8.h"
@@ -252,9 +272,9 @@ Tree_p getUrlData(Context *context, Tree *self,
 
 
 // ============================================================================
-// 
+//
 //    Module glue
-// 
+//
 // ============================================================================
 
 int module_init(const Tao::ModuleApi *api, const Tao::ModuleInfo *)
